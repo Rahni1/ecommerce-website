525b2c01588344e8cb9b02559ea6e5db0f385ce0
RJDdev <rahnidemeis@gmail.com>
2020-06-06 19:42:50 +0200

    Show related products

--- ecommerce-back/routes/product.js
+++ ecommerce-back/routes/product.js
@@ -27,7 +27,7 @@ router.get('/products/search', listSearch)
 router.get('/products/related/:productId', listRelated)
 router.get('/products/categories', listCategories)
 router.post('/products/by/search', listBySearch)
-router.get('/products/photo/:productId', photo)
+router.get('/product/photo/:productId', photo)
 
 
 router.param('userId', userById)
--- ecommerce-front/src/core/Card.js
+++ ecommerce-front/src/core/Card.js
@@ -1,22 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import ShowImage from './ShowImage'
+import moment from 'moment'
 
-const Card = ({product}) => {
+const Card = ({product, showViewProductButton = true }) => {
+    const showViewButton = (showViewProductButton) => {
+        return (
+            showViewProductButton && (
+                <Link to={`/product/${product._id}`} className="mr-2">
+                <button className="btn btn-outline-primary mr-2 mb-2">
+                View Product
+                </button>
+                </Link>
+            )
+        )
+    }
+const showAddToCartButton = () => {
+    return (
+        <button className="btn btn-outline-warning mt-2 mb-2">
+    Add to cart</button>
+    )
+}
+
+const showStock = (quantity) => {
+    return quantity > 0 ? <span className="badge badge-primary badge-pill">In Stock</span>
+     : <span className="badge badge-primary badge-pill">Out of Stock</span>
+}
+
 return (
-    <div className="col-4 mb-3">
     <div className="card">
-    <div className="card-header">{product.name}</div>
+    <div className="card-header name">{product.name}</div>
     <div className="card-body">
     <ShowImage item={product} url="product" />
-    <p>{product.description.substring(0, 100)}</p>
-    <p>${product.price}</p>
-    <Link to="/">
-    <button className="btn btn-outline-primary mt-2 mb-2 mr-2">
-    View Product</button></Link>
-    <button className="btn btn-outline-warning mt-2 mb-2">
-    Add to card</button>
-    </div>
+    <p className="lead mt-2">{product.description.substring(0, 100)}</p>
+    <p className="black-10">${product.price}</p>
+    <p className="black-9">Category: {product.category && product.category.name}</p>
+    <p className="black-8">Added {moment(product.createdAt).fromNow()}</p>
+    {showStock(product.quantity)}
+    <br />
+    {showViewButton(showViewProductButton)}
+    {showAddToCartButton()}
     </div>
     </div>
 )
@@ -24,4 +47,3 @@ return (
 
 export default Card
 
-// next section commit - implement advanced product filtering 
